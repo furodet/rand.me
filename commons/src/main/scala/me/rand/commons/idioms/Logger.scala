fed870058db55e65999a58bbbd2b51cb31e34598
David Furodet <david.furodet@gmail.com>
2018-10-18 06:47:38 +0200

    Create a Logger

---
+++ commons/src/main/scala/me/rand/commons/idioms/Logger.scala
@@ -0,0 +1,134 @@
+/*-
+ * Copyright (c) 2018 rand.me project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package me.rand.commons.idioms
+
+import java.io.PrintWriter
+
+import me.rand.commons.idioms.Logger.LoggerConfiguration
+
+import scala.language.implicitConversions
+
+/** Logs messages issued by programs.
+  *
+  * Idiom to build a Logger is: {{{Logger.forConfiguration(Seq(T -> CONFIG, T -> CONFIG...))}}}
+  * Where {{{CONFIG}}} specifies what to do with each of the following types of message {{{T}}}:
+  * {{{LogError, LogWarning, LogInfo, LogDebug}}} plus the special type {{{LogTrace}}}
+  * which reflects a message traced by the program for further usage.
+  *
+  * Syntax of a {{{CONFIG}}} is {{{PREFIX >>> OUTPUT}}}, where:
+  *   - {{{PREFIX}}} is a string printed before each new message
+  *   - {{{OUTPUT}}} is a {{{PrintWriter}}} (e.g. {{{System.out}}})
+  *
+  * Once configured, a {{{Logger}}} can print a message of a given type with the
+  * following functions:
+  *   - {{{!!}}} : print an error
+  *   - {{{!}}} : print a warning
+  *   - {{{>}}} : print an info
+  *   - {{{~>}}} : print a debug information
+  *   - {{{>>}}} : print a trace
+  *
+  */
+class Logger(configuration: LoggerConfiguration) {
+  private def printIfNeeded(message: String, out: Option[(String, PrintWriter)]): Unit =
+    out match {
+      case None =>
+      // Ignore
+
+      case Some((prefix, printWriter)) =>
+        printWriter.println(prefix + message)
+    }
+
+  def !!(errorMessage: String): Unit = printIfNeeded(errorMessage, configuration.errorOut)
+
+  def !(warningMessage: String): Unit = printIfNeeded(warningMessage, configuration.warningOut)
+
+  def >(infoMessage: String): Unit = printIfNeeded(infoMessage, configuration.infoOut)
+
+  def ~>(traceMessage: String): Unit = printIfNeeded(traceMessage, configuration.traceOut)
+
+  def >>(debugMessage: String): Unit = printIfNeeded(debugMessage, configuration.debugOut)
+}
+
+object Logger {
+
+  sealed trait LogType
+
+  case object LogError extends LogType
+
+  case object LogWarning extends LogType
+
+  case object LogInfo extends LogType
+
+  case object LogDebug extends LogType
+
+  case object LogTrace extends LogType
+
+  class LoggerOutputSpecification(val prefix: String, val writer: PrintWriter)
+
+  class LoggerOutputSpecificationBuilder(prefix: String) {
+    def to(writer: PrintWriter): LoggerOutputSpecification =
+      new LoggerOutputSpecification(prefix, writer)
+  }
+
+  implicit def StringToLoggerOutputSpecificationBuilder(prefix: String): LoggerOutputSpecificationBuilder =
+    new LoggerOutputSpecificationBuilder(prefix)
+
+  class LoggerConfiguration(val errorOut: Option[(String, PrintWriter)],
+                            val warningOut: Option[(String, PrintWriter)],
+                            val infoOut: Option[(String, PrintWriter)],
+                            val debugOut: Option[(String, PrintWriter)],
+                            val traceOut: Option[(String, PrintWriter)]
+                           ) {
+    def setOutput(logType: LogType, output: LoggerOutputSpecification): LoggerConfiguration = {
+      val spec = (output.prefix, output.writer)
+      logType match {
+        case LogError =>
+          new LoggerConfiguration(Some(spec), warningOut, infoOut, debugOut, traceOut)
+        case LogWarning =>
+          new LoggerConfiguration(errorOut, Some(spec), infoOut, debugOut, traceOut)
+        case LogInfo =>
+          new LoggerConfiguration(errorOut, warningOut, Some(spec), debugOut, traceOut)
+        case LogDebug =>
+          new LoggerConfiguration(errorOut, warningOut, infoOut, Some(spec), traceOut)
+        case LogTrace =>
+          new LoggerConfiguration(errorOut, warningOut, infoOut, debugOut, Some(spec))
+      }
+    }
+  }
+
+  private object LoggerConfiguration {
+    def silent = new LoggerConfiguration(None, None, None, None, None)
+  }
+
+  def forConfiguration(configuration: Seq[(LogType, LoggerOutputSpecification)]): Logger = {
+    val loggerConfiguration = configuration.foldLeft(LoggerConfiguration.silent) {
+      case (underConstruction, (eachType, eachOutput)) =>
+        underConstruction.setOutput(eachType, eachOutput)
+    }
+    new Logger(loggerConfiguration)
+  }
+}
+
---
+++ commons/src/test/scala/me/rand/commons/idioms/LoggerTest.scala
@@ -0,0 +1,145 @@
+/*-
+ * Copyright (c) 2018 rand.me project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package me.rand.commons.idioms
+
+import java.io.{PrintWriter, StringWriter}
+
+import me.rand.commons.idioms.Logger._
+import org.scalatest.FlatSpec
+
+class LoggerTest extends FlatSpec {
+
+  class TestWriter {
+    private def newOut = {
+      val writer = new StringWriter()
+      (new PrintWriter(writer), writer)
+    }
+
+    val (errors, errors_) = newOut
+    val (warnings, warnings_) = newOut
+    val (infos, infos_) = newOut
+    val (debugs, debugs_) = newOut
+    val (traces, traces_) = newOut
+
+    def close(): Unit = {
+      errors.close()
+      warnings.close()
+      infos.close()
+      debugs.close()
+      traces.close()
+    }
+
+    def flush(): Unit = {
+      errors.flush()
+      warnings.flush()
+      infos.flush()
+      debugs.flush()
+      traces.flush()
+    }
+  }
+
+  "a logger " should "print errors when asked to" in {
+    val writer = new TestWriter
+    val logger = Logger.forConfiguration(Seq(LogError -> ("E" to writer.errors)))
+    writeMessagesAndAssertThatBufferIsOk("E", writer, logger.!!, writer.errors_)
+  }
+
+  "a logger" should "not print errors when not asked to" in {
+    val writer = new TestWriter
+    val logger = Logger.forConfiguration(Seq())
+    writeMessagesAndAssertThatBufferIsEmpty(writer, logger.!!, writer.errors_)
+  }
+
+  "a logger " should "print warnings when asked to" in {
+    val writer = new TestWriter
+    val logger = Logger.forConfiguration(Seq(LogWarning -> ("W" to writer.warnings)))
+    writeMessagesAndAssertThatBufferIsOk("W", writer, logger.!, writer.warnings_)
+  }
+
+  "a logger" should "not print warnings when not asked to" in {
+    val writer = new TestWriter
+    val logger = Logger.forConfiguration(Seq())
+    writeMessagesAndAssertThatBufferIsEmpty(writer, logger.!!, writer.warnings_)
+  }
+
+  "a logger " should "print infos when asked to" in {
+    val writer = new TestWriter
+    val logger = Logger.forConfiguration(Seq(LogInfo -> ("I" to writer.infos)))
+    writeMessagesAndAssertThatBufferIsOk("I", writer, logger.>, writer.infos_)
+  }
+
+  "a logger" should "not print infos when not asked to" in {
+    val writer = new TestWriter
+    val logger = Logger.forConfiguration(Seq())
+    writeMessagesAndAssertThatBufferIsEmpty(writer, logger.>, writer.infos_)
+  }
+
+  "a logger " should "print debugs when asked to" in {
+    val writer = new TestWriter
+    val logger = Logger.forConfiguration(Seq(LogDebug -> ("D" to writer.debugs)))
+    writeMessagesAndAssertThatBufferIsOk("D", writer, logger.>>, writer.debugs_)
+  }
+
+  "a logger" should "not print debugs when not asked to" in {
+    val writer = new TestWriter
+    val logger = Logger.forConfiguration(Seq())
+    writeMessagesAndAssertThatBufferIsEmpty(writer, logger.>>, writer.debugs_)
+  }
+
+  "a logger " should "print traces when asked to" in {
+    val writer = new TestWriter
+    val logger = Logger.forConfiguration(Seq(LogTrace -> ("//" to writer.traces)))
+    writeMessagesAndAssertThatBufferIsOk("//", writer, logger.~>, writer.traces_)
+  }
+
+  "a logger" should "not print traces when not asked to" in {
+    val writer = new TestWriter
+    val logger = Logger.forConfiguration(Seq())
+    writeMessagesAndAssertThatBufferIsEmpty(writer, logger.~>, writer.traces_)
+  }
+
+  private def writeMessagesAndAssertThatBufferIsOk(expectedPrefix: String,
+                                                   writer: TestWriter,
+                                                   logFunction: String => Unit,
+                                                   getter: => StringWriter): Unit = {
+    logFunction("message 1")
+    logFunction("message 2")
+    writer.flush()
+    assert(getter.toString.contains(expectedPrefix + "message 1"))
+    assert(getter.toString.contains(expectedPrefix + "message 2"))
+    writer.close()
+  }
+
+  private def writeMessagesAndAssertThatBufferIsEmpty(writer: TestWriter,
+                                                      logFunction: String => Unit,
+                                                      getter: => StringWriter): Unit = {
+    logFunction("message 1")
+    logFunction("message 2")
+    writer.flush()
+    assert(getter.toString.isEmpty)
+    writer.close()
+  }
+}
