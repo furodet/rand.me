25bd0a6a8eed4266578b806c2ef1f8a61305bc68
David Furodet <david.furodet@gmail.com>
2018-11-17 20:10:30 +0100

    VM: complete bitflip tests
    No need to test much, so let's focus on the bare minimum.

--- vm/src/test/scala/me/rand/vm/is/BitFlipSpec.scala
+++ vm/src/test/scala/me/rand/vm/is/BitFlipSpec.scala
@@ -31,23 +31,108 @@ import me.rand.vm.dsl.AbstractAsmInstructionBuilder._
 import me.rand.vm.dsl.AbstractAsmOperandBuilder._
 import me.rand.vm.engine.Variable.Scalar
 import me.rand.vm.engine.VmContext
+import me.rand.vm.main.VmError.VmExecutionError.InconsistentOperandTypeError
 
 class BitFlipSpec extends BaseIsSpec {
-  "bitflip" should "pass -1:u32 > %0" in {
+  "bitflip" should "pass ~0xaaaaaaaa:u32 > %0" in {
     implicit val vmContext: VmContext = givenABareMinimalVmContext
     (for {
-      command <- BitFlip < !!(-1, 'u32) > %(0)
+      command <- BitFlip < !!(0xaaaaaaaa, 'u32) > %(0)
       context <- command.execute(vmContext)
       result <- context.heap.getVariable(0)
     } yield result) match {
       case Ok(Some(Scalar(variableName, value))) =>
         assert(variableName == "hp0")
-        assert(value.toInt == 0)
+        assert(value.toInt == 0x55555555)
         assert(value.vmType.isUnsigned)
         assert(value.vmType.byteLen == 4)
 
       case whatever =>
-        fail(s"unexpected result of 'bitflip -1:u32 > %0': $whatever")
+        fail(s"unexpected result of '~0xaaaaaaaa:u32 > %0': $whatever")
+    }
+  }
+
+  "bitflip" should "pass ~%0 > $0" in {
+    implicit val vmContext: VmContext = givenABareMinimalVmContext
+    (for {
+      command <- BitFlip < %(0) > $(0)
+      context <- command.execute(vmContext)
+      result <- context.stack.getVariable(0)
+    } yield result) match {
+      case Ok(Some(Scalar(variableName, value))) =>
+        assert(variableName == "stk0")
+        assert(value.vmType.isUnsigned)
+        assert(value.toInt == ~0x87654321)
+
+      case whatever =>
+        fail(s"unexpected result of '~%0 > $$0': $whatever")
+    }
+  }
+
+  "bitflip" should "pass ~$0 > %0" in {
+    implicit val vmContext: VmContext = givenABareMinimalVmContext
+    (for {
+      command <- BitFlip < $(0) > %(0)
+      context <- command.execute(vmContext)
+      result <- context.heap.getVariable(0)
+    } yield result) match {
+      case Ok(Some(Scalar(variableName, value))) =>
+        assert(variableName == "hp0")
+        assert(value.vmType.isUnsigned)
+        assert(value.toInt == ~0x12345678)
+
+      case whatever =>
+        fail(s"unexpected result of '~$$0 > %0 > %0': $whatever")
+    }
+  }
+
+  "bitflip" should "pass '~**%2 > $0'" in {
+    implicit val vmContext: VmContext = givenABareMinimalVmContext
+    (for {
+      command <- BitFlip < *.*(%(2)) > %(0)
+      context <- command.execute(vmContext)
+      result <- context.heap.getVariable(0)
+    } yield result) match {
+      case Ok(Some(Scalar(variableName, value))) =>
+        assert(variableName == "hp0")
+        assert(value.vmType.isUnsigned)
+        assert(value.toInt == ~0x12345678)
+
+      case whatever =>
+        fail(s"unexpected result of '~**%2 > $$0': $whatever")
+    }
+  }
+
+  "copy" should "pass '~12345:u32 > **%2'" in {
+    implicit val vmContext: VmContext = givenABareMinimalVmContext
+    (for {
+      command <- BitFlip < !!(12345, 'u32) > *.*(%(2))
+      context <- command.execute(vmContext)
+      result <- context.stack.getVariable(0)
+    } yield result) match {
+      case Ok(Some(Scalar(variableName, value))) =>
+        assert(variableName == "stk0")
+        assert(value.toInt == ~12345)
+        assert(value.vmType.isUnsigned)
+        assert(value.vmType.byteLen == 4)
+
+      case whatever =>
+        fail(s"unexpected result of '~12345:u32 > **%2': $whatever")
+    }
+  }
+
+  "bitflip" should "pass '~%1 > %0 (pointer to instruction)'" in {
+    implicit val vmContext: VmContext = givenABareMinimalVmContext
+    (for {
+      command <- BitFlip < %(1) > %(0)
+      context <- command.execute(vmContext)
+      result <- context.heap.getVariable(0)
+    } yield result) match {
+      case Err(err: InconsistentOperandTypeError) =>
+        println(err)
+
+      case whatever =>
+        fail(s"unexpected result of '~$$2 > %0 (pointer to instruction)': $whatever")
     }
   }
 }
